9078351eced6a2f963a037ae2c92287284db47f6
Shlomi Ganon <Shlomi1395@Gmail.com>
2026-05-08 21:10:15 +0300

    remove fake login page from recent work (because we will adding js) , rename main_menu_script.js to login_menu_script.js

--- js/login_menu_script.js
+++ js/login_menu_script.js
@@ -1,10 +1,13 @@
 const loginBtn = document.getElementById('login-button');
 const getCodeBtn = document.getElementById('get-code-button');
+const rememberMeCheckbox = document.getElementById('remember-me-checkbox');
+const forgotPassButton = document.getElementById('forgot-pass-button');
+
 const emailInput = document.getElementById('email-field');
 const passwordInput = document.getElementById('password-field');
-const rememberMeCheckbox = document.getElementById('remember-me-checkbox');
 loginBtn.addEventListener('click', Login_Click);
 getCodeBtn.addEventListener('click', GetCode_Click);
+forgotPassButton.addEventListener('click', ForgotPass_Click);
 
 function Login_Click() 
 {
@@ -20,4 +23,9 @@ function GetCode_Click()
     const msg = "get-code-information ->\n" + 
     "email: [" + emailInput.value + "]";
     alert(msg);
+}
+
+function ForgotPass_Click() 
+{
+    window.location.href = '../html/no_support.html';
 }
